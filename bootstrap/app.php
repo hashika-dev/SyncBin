17ce6bb2c21cb0bc3f95b185aaa718e65bca594d
hashika-dev <kurtumali06@gmail.com>
2026-07-23 14:29:53 +0800

    fix: trust reverse proxy headers and allow GET fallback for logout to resolve 419 Page Expired

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,6 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
+        $middleware->trustProxies(at: '*');
         $middleware->alias([
             '2fa' => \Laragear\TwoFactor\Http\Middleware\ConfirmTwoFactorCode::class,
             'role' => \App\Http\Middleware\EnsureUserHasRole::class,
